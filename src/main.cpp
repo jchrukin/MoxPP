6a19a1cec25e6740a0ccf29d4c1e0eb1130ca47d
jchrukin <jchrukin@blees.co>
2025-04-14 00:00:25 +0200

    Added bazel files allowing to compile main,cpp file.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,14 @@
 #include <spdlog/spdlog.h>
+#include <iostream>
 
 int main()
 {
+	int test;
+	
     // We will use spdlog to say hello! 
     // So that you can see that conan works :-)
     spdlog::warn("Hello World from version {}!", MOXPP_VERSION);
+	
+	//Stop the program to see the output
+	std::cin >> test;
 }
